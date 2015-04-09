4e32ff06064fc6a7e9465857f0bc0f9fcf1064ed
Sidnei Sanches <sidneipsj@gmail.com>
2015-04-09 17:06:11 -0300

    First commit

---
+++ ProjetosTeste/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetosTeste.Startup))]
+namespace ProjetosTeste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
